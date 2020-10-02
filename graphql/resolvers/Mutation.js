c49fe879ab0b7e9a0d03217bf7e65d7cf80c4998
Charles Nguyen <nguyencharles@live.ca>
2020-10-01 22:03:39 -0400

    Removed .idea folder since it should be ignored

--- graphql/resolvers/Mutation.js
+++ graphql/resolvers/Mutation.js
@@ -28,6 +28,9 @@ async function updateClient(parent, args, context, info) {
       id: Number(args.id),
     },
   });
+
+  if (foundUser) throw new Error(`Receipt with id ${args.id} not found.`);
+
   return await context.prisma.client.update({
     where: {
       id: Number(args.id),
@@ -48,10 +51,150 @@ async function updateClient(parent, args, context, info) {
   });
 }
 
+async function addProduce(parent, args, context, info) {
+  return await context.prisma.produce.create({
+    data: {
+      name: args.name,
+      quantity: args.quantity,
+      price: args.price,
+      foodtype: args.foodtype,
+    },
+  });
+}
 
+async function deleteProduce(parent, args, context, info) {
+  return await context.prisma.produce.delete({
+    where: {
+      id: Number(args.id),
+    },
+  });
+}
+
+async function updateProduce(parent, args, context, info) {
+  const foundProduce = context.prisma.produce.findOne({
+    where: {
+      id: Number(args.id),
+    },
+  });
+
+  if (foundProduce) throw new Error(`Receipt with id ${args.id} not found.`);
+
+  return await context.prisma.produce.update({
+    where: {
+      id: Number(args.id),
+    },
+    data: {
+      quantity:
+        foundProduce.quantity === args.quantity
+          ? foundProduce.quantity
+          : args.quantity,
+      price:
+        foundProduce.price === args.price ? foundProduce.price : args.price,
+    },
+  });
+}
+
+async function addStore(parent, args, context, info) {
+  return await context.prisma.store.create({
+    data: {
+      name: args.name,
+      address: args.address,
+      email: args.email,
+      storeowner: args.storeowner,
+      city: args.city,
+      province: args.province,
+    },
+  });
+}
+
+async function deleteStore(parent, args, context, info) {
+  return await context.prisma.store.delete({
+    where: {
+      id: Number(args.id),
+    }
+  });
+}
+
+async function updateStore(parent, args, context, info) {
+  const foundStore = await context.prisma.store.findOne({
+    where: {
+      id: Number(args.id),
+    },
+  });
+
+  if (foundStore) throw new Error(`Receipt with id ${args.id} not found.`);
+
+  return await context.prisma.store.update({
+    where: {
+      id: Number(args.id),
+    },
+    data: {
+      name: foundStore.name === args.name ? foundStore.name : args.name,
+      address:
+        foundStore.address === args.address ? foundStore.address : args.address,
+      email: foundStore.email === args.email ? foundStore.email : args.email,
+      storeowner:
+        foundStore.storeowner === args.storeowner
+          ? foundStore.storeowner
+          : args.storeowner,
+      city: foundStore.city === args.city ? foundStore.city : args.city,
+      province:
+        foundStore.province === args.province
+          ? foundStore.province
+          : args.province,
+    },
+  });
+}
+
+//! Should have client, store and produces
+async function addReceipt(parent, args, context, info) {
+  return await context.prisma.receipt.create({
+    data: {
+      subtotal: args.subtotal,
+      total: args.total,
+    },
+  });
+}
+
+async function deleteReceipt(parent, args, context, info) {
+  return await context.prisma.receipt.delete({
+    where: {
+      id: Number(args.id),
+    }
+  });
+}
+
+async function updateReceipt(parent, args, context, info) {
+  const foundReceipt = await context.prisma.receipt.findOne({
+    where: {
+      id: Number(args.id),
+    },
+  });
+
+  if (foundReceipt) throw new Error(`Receipt with id ${args.id} not found.`);
+
+  return await context.prisma.receipt.update({
+    where: {
+      id: Number(args.id),
+    },
+    data: {
+      subtotal: foundReceipt.subtotal === args.subtotal ? foundReceipt.subtotal : args.subtotal,
+      total: foundReceipt.total === args.total ? foundReceipt.total : args.total,
+    },
+  })
+}
 
 module.exports = {
   addClient,
   deleteClient,
   updateClient,
+  addProduce,
+  deleteProduce,
+  updateProduce,
+  addStore,
+  deleteStore,
+  updateStore,
+  addReceipt,
+  updateReceipt,
+  deleteReceipt,
 };
--- graphql/resolvers/Query.js
+++ graphql/resolvers/Query.js
@@ -1,21 +1,58 @@
-function getClients(parent, args, context, info) {
-  return context.prisma.client.findMany();
+async function getClients(parent, args, context, info) {
+  return await context.prisma.client.findMany();
 }
 
 async function getClient(parent, args, context, info) {
   return await context.prisma.client.findOne({
     where: {
       id: Number(args.id),
-    }
+    },
   });
 }
 
-function getProduces(parent, args, context, info) {
-  return context.prisma.produce.findMany();
+async function getProduces(parent, args, context, info) {
+  return await context.prisma.produce.findMany();
+}
+
+async function getProduce(parent, args, context, info) {
+  return await context.prisma.produce.findOne({
+    where: {
+      id: Number(args.id),
+    },
+  });
+}
+
+async function getStore(parent, args, context, info) {
+  return await context.prisma.store.findOne({
+    where: {
+      id: Number(args.id),
+    },
+  });
+}
+
+async function getStores(parent, args, context, info) {
+  return await context.prisma.store.findMany();
+}
+
+async function getReceipt(parent, args, context, info) {
+  return await context.prisma.receipt.findOne({
+    where: {
+      id: Number(args.id),
+    }
+  })
+}
+
+async function getReceipts(parent, args, context, info) {
+  return await context.prisma.receipt.findMany();
 }
 
 module.exports = {
   getClients,
   getClient,
   getProduces,
-}
+  getProduce,
+  getStore,
+  getStores,
+  getReceipt,
+  getReceipts,
+};
