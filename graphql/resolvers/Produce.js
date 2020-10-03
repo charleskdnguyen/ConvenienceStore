1c342e3640a1c6d533aed9733158d8f4ac42586d
Charles Nguyen <nguyencharles@live.ca>
2020-10-03 16:44:09 -0400

    Renamed list of receipts function in produce resolver to proper name

--- graphql/resolvers/Produce.js
+++ graphql/resolvers/Produce.js
@@ -8,7 +8,7 @@ async function store(parent, args, context, info) {
     .store();
 }
 
-async function receipt(parent, args, context, info) {
+async function receiptscontainingproduce(parent, args, context, info) {
   return await context.prisma.produce
     .findOne({
       where: {
@@ -20,5 +20,5 @@ async function receipt(parent, args, context, info) {
 
 module.exports = {
   store,
-  receipt,
+  receiptscontainingproduce,
 };
